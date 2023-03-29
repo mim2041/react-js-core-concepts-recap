a14846c5983ad3d444af6f95678e5b4ce4933dcd
mim2041 <suraiyamim338@gmail.com>
2023-03-29 12:02:50 +0600

    Boost the power using useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -12,17 +13,21 @@ function App() {
 }
 
 const districtStyle = {
-  backgroundColor: 'gray',
+  backgroundColor: 'skyblue',
   margin: '20px',
   borderRadius: '20px',
   padding: '15px'
 }
 
 function District(props){
+  const [power, setPower] = useState(1);
+  const boostPower = () => setPower(power * 2);
   return (
     <div style={districtStyle}>
       <h2>Name: {props.name}</h2>
       <p>Specialty: {props.specialty}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Boost The Power</button>
     </div>
   )
 }
