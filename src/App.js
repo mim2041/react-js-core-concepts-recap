0108b7f196b6c699a52b286c1600d94d6d8570de
mim2041 <suraiyamim338@gmail.com>
2023-03-29 11:51:25 +0600

    react js core concepts recap by simple props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name='Noakhali' specialty= 'Bivag'></District>
+      <District name='Bramonbaria' specialty='joda akbar'></District>
+      <District name='Sumilla' specialty='Moyna and Moti'></District>
     </div>
   );
+}
+
+const districtStyle = {
+  backgroundColor: 'gray',
+  margin: '20px',
+  borderRadius: '20px',
+  padding: '15px'
+}
+
+function District(props){
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.specialty}</p>
+    </div>
+  )
 }
 
 export default App;
